d978aba34a82da0a10a1e82f57921e012059ad90
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-07-28 11:41:18 -0300

    dbs e containers terraform

--- Terraform/images.tf
+++ Terraform/images.tf
@@ -1,28 +1,9 @@
-variable "build_images_command" {
-  default = <<-EOT
-        docker build -t bootcampici.azurecr.io/newsconsumerapi ./NewsConsumer/API && 
-        docker build -t bootcampici.azurecr.io/newsconsumerapi ./NewsConsumer/Proxy && 
-        docker build -t bootcampici.azurecr.io/analyzecommentsapi ./AnalyzeComments/API && 
-        docker build -t bootcampici.azurecr.io/analyzecommentsapi ./AnalyzeComments/Proxy
-      EOT
-}
-
 resource "null_resource" "build_images" {
   provisioner "local-exec" {
     working_dir = "${path.module}/../"
-    command     = chomp(var.build_images_command)
-    interpreter = ["sh", "-c"]
+    command     = "docker build -t bootcampici.azurecr.io/newsconsumerapi ./NewsConsumer/API && docker build -t bootcampici.azurecr.io/newsconsumerproxy ./NewsConsumer/Proxy && docker build -t bootcampici.azurecr.io/analyzecommentsapi ./AnalyzeComments/API && docker build -t bootcampici.azurecr.io/analyzecommentsproxy ./AnalyzeComments/Proxy"
+    # interpreter = ["sh", "-c"]
   }
-}
-
-variable "upload_images_command" {
-  default = <<-EOT
-        az acr login --name bootcampici &&
-        docker push bootcampici.azurecr.io/newsconsumerapi:latest &&
-        docker push bootcampici.azurecr.io/newsconsumerproxy:latest &&
-        docker push bootcampici.azurecr.io/analyzecommentsapi:latest &&
-        docker push bootcampici.azurecr.io/analyzecommentsproxy:latest
-      EOT
 }
 
 resource "null_resource" "upload_images" {
@@ -32,8 +13,8 @@ resource "null_resource" "upload_images" {
 
   provisioner "local-exec" {
     working_dir = "${path.module}/../"
-    command     = chomp(var.upload_images_command)
-    interpreter = ["sh", "-c"]
+    command     = "az acr login --name bootcampici && docker push bootcampici.azurecr.io/newsconsumerapi:latest && docker push bootcampici.azurecr.io/newsconsumerproxy:latest && docker push bootcampici.azurecr.io/analyzecommentsapi:latest && docker push bootcampici.azurecr.io/analyzecommentsproxy:latest"
+    # interpreter = ["sh", "-c"]
   }
 
   depends_on = [
